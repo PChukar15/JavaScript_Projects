2d16ce58bca5020765c56badb88c187c9f9c6e1d
PChukar15 <160537294+PChukar15@users.noreply.github.com>
2024-04-25 08:45:14 -0700

    initial JavaScript

---
+++ Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,5 @@
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string"
+
+window.alert(Sent1); //prints first variable
+document.write(Sent1 + Sent2); //Concatenates both variables together
