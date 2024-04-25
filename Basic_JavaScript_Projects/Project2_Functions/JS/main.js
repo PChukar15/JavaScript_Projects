a469000b9afeab9ef97dcf9689f003ad2bf09478
PChukar15 <160537294+PChukar15@users.noreply.github.com>
2024-04-25 11:14:51 -0700

    Project 2

---
+++ Basic_JavaScript_Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() { //begins function
+    var sentence = "This is part one,"; //defines variable
+    sentence += " and this is part two"; //adds onto variable
+    document.getElementById("Combine").innerHTML=sentence; //creates HTML element that calls function
+}
